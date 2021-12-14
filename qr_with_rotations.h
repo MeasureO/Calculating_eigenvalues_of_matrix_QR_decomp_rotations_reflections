7d2a8f351903e112bf220e638f84d5ac5cdc15c3
measure0 <fedotov.dmitry.sergeevich@gmail.com>
2021-12-14 18:18:04 +0300

    fix

--- qr_with_rotations.h
+++ qr_with_rotations.h
@@ -20,9 +20,9 @@ Matrix qr_rotations(Matrix matrix, int size)
     to_identity(Ts, size);
     // std::cout << Ts;
     // std::cout << "-------------------------------------------\n";
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < 5; j++)
     {
-        matrix = with_reflections_to_quasi_triangular(matrix, size);
+        // matrix = with_reflections_to_quasi_triangular(matrix, size);
         std::cout << "quasi triangular\n";
         // std::cout << matrix;
         print_matrix(matrix, size, size);
@@ -30,6 +30,7 @@ Matrix qr_rotations(Matrix matrix, int size)
         {
             std::cout << k + 1 << "step of rotation\n";
             to_identity(T, size);
+            // to_identity(Ts, size);
             double cosa = matrix(k, k) / sqrt(matrix(k, k) * matrix(k, k) + matrix(k + 1, k) * matrix(k + 1, k));
             double sina = -matrix(k + 1, k) / sqrt(matrix(k, k) * matrix(k, k) + matrix(k + 1, k) * matrix(k + 1, k));
             T(k, k) = cosa;
@@ -53,12 +54,29 @@ Matrix qr_rotations(Matrix matrix, int size)
         }
         Q = transpose(Ts, size);
         R = matrix;
+        std::cout << j + 1 << " ITERATION QR\n";
+        print_matrix(multiplication(Q, R, size), size, size);
         matrix = multiplication(R, Q, size);
-        std::cout << "A_(k+1)\n";
+        std::cout << j + 1 << " ITERATION A_(k+1) = RQ\n";
         // std::cout << matrix;
         print_matrix(matrix, size, size);
         std::cout << "------------------------------------" << std::endl;
+        to_identity(Ts, size);
     }
     std::cout << "**************************************************************\n";
     std::cout << matrix;
+    printf("\033[0;31m");      // Set the text to the color red
+    printf("EIGENVALUES: \n"); // Display Hello in red
+    printf("\033[0m");         // Resets the text to default color
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == j)
+            {
+                printf("%10.3f", matrix(i, j));
+            }
+        }
+    }
+    printf("\n");
 }
